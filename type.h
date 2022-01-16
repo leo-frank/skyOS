edca94895c438cdec0fd9aea366eac27e1c53108
Wynn <1052209675@qq.com>
2022-01-17 00:56:50 +0800

    refactor(type): uint type
    Signed-off-by: Wynn <1052209675@qq.com>

--- type.h
+++ type.h
@@ -1,7 +1,8 @@
 #pragma once
 
-typedef unsigned uint;
-typedef unsigned long uint64;
+typedef __UINT16_TYPE__ uint16;
+typedef __UINT32_TYPE__ uint;
+typedef __UINT64_TYPE__ uint64;
 typedef __SIZE_TYPE__ size_t;
 typedef __PTRDIFF_TYPE__ ptrdiff_t;
 #define NULL ((void*)0)
