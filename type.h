8e040c57f8703768a8694b1ea8ce89fe8e005de9
Wynn <1052209675@qq.com>
2022-01-25 20:08:53 +0800

    feat(type): add 2 basic type
    Signed-off-by: Wynn <1052209675@qq.com>

--- type.h
+++ type.h
@@ -1,7 +1,9 @@
 #pragma once
 
+typedef __UINT8_TYPE__ uint8;
 typedef __UINT16_TYPE__ uint16;
 typedef __UINT32_TYPE__ uint;
+typedef __UINT32_TYPE__ uint32;
 typedef __UINT64_TYPE__ uint64;
 typedef __SIZE_TYPE__ size_t;
 typedef __PTRDIFF_TYPE__ ptrdiff_t;
