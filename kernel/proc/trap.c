24b44701e48adf4fbab49801d5662ca6d5435331
Wynn <1052209675@qq.com>
2022-02-01 02:57:10 +0800

    fix(trap): make sure trap return correctly
    I forget to save register info when we enter in trap, which is a stupid
    error. but in `make run`` mode, where traps like time interrupt doesn't
    happen(this can be confirmed by set log_info level at debug but no timer
    interrupt debug info been displayed), even we forget save regs, no
    errors will been told. In `make debug` mode, timer interrupt happens,
    and tells me a diffrent behavior between `make run mode`.
    That's the error effect of `FORGETTING SAVE REG INFO BEFORE TRAP`.
    However, now this is a very primary version of reg save, more details to
    add.
    
    Signed-off-by: Wynn <1052209675@qq.com>

--- kernel/proc/trap.c
+++ kernel/proc/trap.c
@@ -9,11 +9,52 @@
 // define in entry.S
 void trap_entry();
 
-extern char sscratch_stack_top[];
+extern char sscratch_stack[];
+
+struct trapframe {
+  /* NOT USED */
+  /*   0 */ uint64 kernel_satp;  // kernel page table
+  /*   8 */ uint64 kernel_sp;    // top of process's kernel stack
+  /*  16 */ uint64 kernel_trap;  // usertrap() 切换到内核后我们会跳到这里执行。
+  /*  24 */ uint64 epc;            // saved user program counter
+  /*  32 */ uint64 kernel_hartid;  // saved kernel tp
+
+  /*  40 */ uint64 ra;
+  /*  48 */ uint64 sp;
+  /*  56 */ uint64 gp;
+  /*  64 */ uint64 tp;
+  /*  72 */ uint64 t0;
+  /*  80 */ uint64 t1;
+  /*  88 */ uint64 t2;
+  /*  96 */ uint64 s0;
+  /* 104 */ uint64 s1;
+  /* 112 */ uint64 a0;
+  /* 120 */ uint64 a1;
+  /* 128 */ uint64 a2;
+  /* 136 */ uint64 a3;
+  /* 144 */ uint64 a4;
+  /* 152 */ uint64 a5;
+  /* 160 */ uint64 a6;
+  /* 168 */ uint64 a7;
+  /* 176 */ uint64 s2;
+  /* 184 */ uint64 s3;
+  /* 192 */ uint64 s4;
+  /* 200 */ uint64 s5;
+  /* 208 */ uint64 s6;
+  /* 216 */ uint64 s7;
+  /* 224 */ uint64 s8;
+  /* 232 */ uint64 s9;
+  /* 240 */ uint64 s10;
+  /* 248 */ uint64 s11;
+  /* 256 */ uint64 t3;
+  /* 264 */ uint64 t4;
+  /* 272 */ uint64 t5;
+  /* 280 */ uint64 t6;
+};
 
 void trap_init() {
   // stack that reserve trap context
-  sscratch_set((unsigned long)(sscratch_stack_top));
+  sscratch_set((unsigned long)(sscratch_stack));
   // set the supervisor trap handler
   stvec_set((unsigned long)trap_entry);
   // enable supervisor interrupts
@@ -51,8 +92,10 @@ char *exception_code_description[] = {"Instruction address misaligned",
                                       "Store/AMO page fault"};
 
 extern void do_timer();
+extern void syscall();
+extern void trap_return(struct trapframe *a0);
 
-void trap_start() {
+void trap_start(struct trapframe *a0) {
   uint64 tval = stval_get();
   uint64 sepc = sepc_get();
   uint64 cause = scause_get();
@@ -77,9 +120,18 @@ void trap_start() {
       case ILLEGAL_INSTRUCTION:
         log_debug("tval = %p", tval);
         log_debug("sepc = %p", sepc);
+        panic("ILLEGAL_INSTRUCTION");
         break;
+      case SYSCALL_FROM_U_MODE:
+        // syscall();
+        break;
+      default:
+        log_debug("tval = %p", tval);
+        log_debug("sepc = %p", sepc);
+        panic("UNEXPECTED EXCEPTION");
     }
     sepc_set(sepc + 4);
   }
+  trap_return(a0);
   return;
 }
